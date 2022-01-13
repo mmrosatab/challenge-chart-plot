f3009eb6c2d90a8ca254ee7d3ba449480dcc4eca
mmrosatab <mmrosatab@hotmail.com>
2022-01-13 07:53:43 -0300

    Docs and Folder: Updating readme file and removing unnecessary comments

--- README.md
+++ README.md
@@ -20,26 +20,16 @@ The tests were performed with some sets of input tests found in the [test.txt](h
 
 ### An example of entry and exit for the happy day
 
-###### {type: 'start', timestamp: 1519862400000, select: ['min_response_time', 'max_response_time'], group:['os', 'browser']}
-
-###### {type: 'span', timestamp: 1519862400000, begin: 1519862400000, end: 1519862460000}
-
-###### {type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
-
-###### {type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'chrome', min_response_time: 0.2, max_response_time: 1.2}
-
-###### {type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.2}
-
-###### {type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'firefox', min_response_time: 0.1, max_response_time: 1.0}
-
-###### {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.2, max_response_time: 0.9}
-
-###### {type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.0}
-
-###### {type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'firefox', min_response_time: 0.2, max_response_time: 1.1}
-
-###### {type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.4}
-
-###### {type: 'stop', timestamp: 1519862460000}
+{type: 'start', timestamp: 1519862400000, select: ['min_response_time', 'max_response_time'], group:['os', 'browser']}
+{type: 'span', timestamp: 1519862400000, begin: 1519862400000, end: 1519862460000}
+{type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.3}
+{type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'chrome', min_response_time: 0.2, max_response_time: 1.2}
+{type: 'data', timestamp: 1519862400000, os: 'mac', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.2}
+{type: 'data', timestamp: 1519862400000, os: 'linux', browser: 'firefox', min_response_time: 0.1, max_response_time: 1.0}
+{type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'chrome', min_response_time: 0.2, max_response_time: 0.9}
+{type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'chrome', min_response_time: 0.1, max_response_time: 1.0}
+{type: 'data', timestamp: 1519862460000, os: 'mac', browser: 'firefox', min_response_time: 0.2, max_response_time: 1.1}
+{type: 'data', timestamp: 1519862460000, os: 'linux', browser: 'firefox', min_response_time: 0.3, max_response_time: 1.4}
+{type: 'stop', timestamp: 1519862460000}
 
 ![Captura de tela de 2019-04-02 14-51-56](https://user-images.githubusercontent.com/9852787/58393770-96412b80-8016-11e9-88d3-9eaee8b55cfd.png)
--- chat-plot/src/util/TreadUserInput.js
+++ chat-plot/src/util/TreadUserInput.js
@@ -1,92 +1,61 @@
-// This function return an array of string Json. 
-function createJsonString(str)
-{   
-    let str1 = str.trim();
-    let array = str1.split('\n');
-    
-    for(let i=0; i<array.length;i++)
-    {
-        array[i] = eval("("+(array[i])+")");
-    }
+function createJsonString(str) {
+  let str1 = str.trim();
+  let array = str1.split("\n");
 
-    return array;
+  for (let i = 0; i < array.length; i++) {
+    array[i] = eval("(" + array[i] + ")");
+  }
+
+  return array;
 }
 
-// This function checks whether a timestamp passed as an argument is within a valid range.
-function verifyTimestamp(tInterval, tCurrent)
-{
-  return ((tCurrent >= tInterval[0]) && (tCurrent <= tInterval[1]));
+function verifyTimestamp(tInterval, tCurrent) {
+  return tCurrent >= tInterval[0] && tCurrent <= tInterval[1];
 }
 
-// This function return index of lattest start type event 
-function startIndex(array)
-{
-  for(let i=array.length-1;i>=0;i--)
-  {
-    if (array[i].type === 'start')
-    {
+function startIndex(array) {
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i].type === "start") {
       return i;
     }
   }
   return -1;
 }
 
-/* This function is used when an span event is called for 
-update list of datas removing index that timestamp is out new interval. */
-function updateDatas(newInterval, chartData1)
-{
-  for(let i=0; i<chartData1.length; i++)
-  {
-    if(!verifyTimestamp(newInterval,chartData1[i][0]))
-    {
-      alert("timestamp local: "+chartData1[i][0]);
-      alert("tam vector"+ chartData1.length);
-      chartData1.splice(i,1);
-      alert("tam vector"+ chartData1.length);
+function updateDatas(newInterval, chartData1) {
+  for (let i = 0; i < chartData1.length; i++) {
+    if (!verifyTimestamp(newInterval, chartData1[i][0])) {
+      chartData1.splice(i, 1);
     }
   }
 }
 
-// This function generate a dictionary that have datas e categories for chart plot.
-function createLabelCategorie(sJson, group1, select1, timestamp1, chartData1)
-{
-  let label = ""; // variable for concatenation between strings of group
-  let labelgroup = []; // list for concatenation between strings of group and strings of select
+function createLabelCategorie(sJson, group1, select1, timestamp1, chartData1) {
+  let label = "";
+  let labelgroup = [];
 
-  for (let i=0; i< group1.length; i++)
-  {
+  for (let i = 0; i < group1.length; i++) {
     label = label + " " + sJson[group1[i]];
   }
 
-  for (let i=0; i< select1.length; i++)
-  {
+  for (let i = 0; i < select1.length; i++) {
     labelgroup[i] = label + " " + select1[i];
     labelgroup[i] = labelgroup[i].replace(/_/g, " ");
-    //dic[labelgroup[i]] = sJson[select1[i]];
-    //alert(timestamp1+"_"+labelgroup[i]+"_"+sJson[select1[i]]);
-    chartData1.push([timestamp1, labelgroup[i], sJson[select1[i]]]); 
+    chartData1.push([timestamp1, labelgroup[i], sJson[select1[i]]]);
   }
 }
 
-// This function return a timestamp in format of minutes
-function minInterval(timestamp)
-{
+function minInterval(timestamp) {
   let date = new Date(timestamp);
   let timeString = date.toISOString().substr(11, 5);
   return timeString;
-  
 }
 
-// This function return the essencial datas for plot chart
-function preparDataChart(str)
-{
+function preparDataChart(str) {
   let array = createJsonString(str);
   let index = startIndex(array);
 
-  if (index < 0)
-  {
-    // no event start
-    alert("No start event was found");
+  if (index < 0) {
     return false;
   }
 
@@ -94,111 +63,81 @@ function preparDataChart(str)
   let select = array[index].select;
   let group = array[index].group;
   let tGlobal = [];
-  let dict = {"span": false,"data": false};
-  
-  for(let i=index+1; i < array.length; i++)
-  {
-    switch (array[i].type)
-    {
-      case 'span':
-               
-        // update timestamp
-        tGlobal = [array[i].begin , array[i].end];
+  let dict = { span: false, data: false };
 
-        if(dict['span'])
-        {
-          // update data
-          if(chartData.length > 0)
-          {
-            alert("update data");
-            updateDatas(tGlobal,chartData);
+  for (let i = index + 1; i < array.length; i++) {
+    switch (array[i].type) {
+      case "span":
+        tGlobal = [array[i].begin, array[i].end];
+
+        if (dict["span"]) {
+          if (chartData.length > 0) {
+            updateDatas(tGlobal, chartData);
           }
-  
-        }else
-        {
-          dict['span'] = true;
-          //alert(dict['span']);
-        }
-        
-        break;
-
-      case 'data':
-        
-        if((dict['span']) && (verifyTimestamp(tGlobal, array[i].timestamp)))
-        {
-          createLabelCategorie(array[i], group, select, array[i].timestamp, chartData);
+        } else {
+          dict["span"] = true;
         }
 
         break;
 
-      case 'stop':
-        
-        if (chartData.length == 0)
-        {
-          alert("There is no data to plot");
+      case "data":
+        if (dict["span"] && verifyTimestamp(tGlobal, array[i].timestamp)) {
+          createLabelCategorie(
+            array[i],
+            group,
+            select,
+            array[i].timestamp,
+            chartData
+          );
+        }
+
+        break;
+
+      case "stop":
+        if (chartData.length == 0) {
           return false;
         }
 
         let aux1 = {};
         let aux2 = {};
-        let list = []
+        let list = [];
 
-        for(let i=0;i<chartData.length;i++)
-        {
-          try
-          {
+        for (let i = 0; i < chartData.length; i++) {
+          try {
             aux1[chartData[i][1]].push(chartData[i][2]);
-
-          }catch
-          {
+          } catch {
             aux1[chartData[i][1]] = [chartData[i][2]];
           }
-
         }
-        
-        // transform datas in js object
-        for(let i in aux1)
-        {
-          try
-          {
-            aux2[i].data = aux1[i];
 
-          }catch
-          {
-            aux2[i] = {name: i, data: []};
+        for (let i in aux1) {
+          try {
+            aux2[i].data = aux1[i];
+          } catch {
+            aux2[i] = { name: i, data: [] };
             aux2[i].data = aux1[i];
             list.push(aux2[i]);
-
           }
-          
         }
 
-        // get format minutes of timestamp
         let t1 = minInterval(tGlobal[0]);
         let t2 = minInterval(tGlobal[1]);
-        
-        return{
-          categories: [t1,t2],
-          series: list
+
+        return {
+          categories: [t1, t2],
+          series: list,
         };
 
       default:
-        //alert(array[i].type);
-        alert("Invalid event");
         return false;
-
     }
   }
-  
 }
 
-// This function print data of series and categories of chartplot for verification
-function printData(aux)
-{
-  for(let i in aux)
-  {
-    alert("Key: "+i+" Value: "+aux[i]);
+function printData(aux) {
+  for (let i in aux) {
+    alert("Key: " + i + " Value: " + aux[i]);
   }
 }
 
-export {preparDataChart}
+export { preparDataChart };
